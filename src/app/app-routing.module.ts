63505fcd1fc2544f339fd2f7d0a13166a645d9da
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-02 12:59:58 +0530

    craets routing, adds login page and dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {LoginPageComponent} from './login-page/login-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {path: 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
